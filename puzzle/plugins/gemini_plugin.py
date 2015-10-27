cc7c68860d80ac9bb9fb125d5809f03e7195e5b5
moonso <monsunas@gmail.com>
2015-10-27 14:27:40 +0100

    Fixed gemini plugin and added tests form gemini plugin

--- puzzle/plugins/gemini_plugin.py
+++ puzzle/plugins/gemini_plugin.py
@@ -4,7 +4,7 @@ from sqlite3 import OperationalError
 
 from gemini import GeminiQuery
 from puzzle.plugins import Plugin
-from puzzle.models import (Variant, Genotype, Gene, Transcript, Case)
+from puzzle.models import (Variant, Genotype, Gene, Transcript, Case, Individual)
 from puzzle.utils import (get_omim_number, get_ensembl_id, get_hgnc_symbols,
                           get_most_severe_consequence)
 from puzzle.exceptions import DatabaseError
@@ -16,55 +16,84 @@ class GeminiPlugin(Plugin):
 
     def __init__(self):
         super(GeminiPlugin, self).__init__()
-
+        
+        
     def init_app(self, app):
         """Initialize plugin via Flask."""
         logger.debug("Updating root path to {0}".format(
             app.config['PUZZLE_ROOT']
         ))
-        self.root_path = app.config['PUZZLE_ROOT']
+        self.db = app.config['PUZZLE_ROOT']
         logger.debug("Updating pattern to {0}".format(
-            app.config['PUZZLE_PATTERN']
+            app.config['PUZZLE_ROOT']
         ))
-        self.pattern = app.config['PUZZLE_PATTERN']
+        self.case_objs = []
+        self.individuals = []
+        
+        case_ids = set()
+        logger.info("Looking for cases in {0}".format(
+            self.db
+        ))
+        gq = GeminiQuery(self.db)
+        #Dictionaru with sample to index in the gemini database
+        sample_to_idx = gq.sample_to_idx
+        
+        query = "SELECT * from samples"
+        gq.run(query)
+        for individual in gq:
+            logger.info("Found individual {0} with family id {1}".format(
+                individual['name'], individual['family_id']))
+            self.individuals.append(
+                Individual(
+                    ind_id=individual['name'], 
+                    case_id=individual['family_id'], 
+                    mother=individual['maternal_id'], 
+                    father=individual['paternal_id'],
+                    sex=individual['sex'],
+                    phenotype=individual['sex'],
+                    index=sample_to_idx.get(individual['name']), 
+                    variant_source=self.db, 
+                    bam_path=None)
+            )
+            case_ids.add(individual['family_id'])
+        
+        # Add the cases to the adapter
+        for case_id in case_ids:
+            case = Case(
+                case_id=case_id,
+                name=case_id
+                )
+            # Add the individuals to the correct case
+            for individual in self.individuals:
+                if individual['case_id'] == case_id:
+                    logger.info("Adding ind {0} to case {1}".format(
+                        individual['name'], individual['case_id']
+                    ))
+                    case.add_individual(individual)
+            logger.info("Adding case {0} to adapter.".format(case_id))
+            self.case_objs.append(case)
+        
     
     def cases(self, pattern=None):
         """Return all cases."""
-        db = pattern or self.root_path
-        logger.debug("Looking for cases in {0}".format(
-            db
-        ))
-        cases = set()
         
-        gq = GeminiQuery(db)
-        query = "SELECT * from samples"
-        gq.run(query)
-    
-        for individual in gq:
-            logger.debug("Found individual  {0}".format(individual['name']))
-            cases.add(individual['family_id'])
-        
-        logger.debug("Found cases {0}".format(', '.join(cases)))
-        case_objs = (Case(case_id=db.replace('/', '|'),
-                          name = case) for case in cases)
-        
-        return case_objs
+        return self.case_objs
 
-    def _get_individuals(self, gemini_variant, sample2idx):
-        """Add the genotypes for all individuals
+    def _get_individuals(self, gemini_variant, individual_objs):
+        """Add the genotypes for a variant for all individuals
         
             Args:
                 gemini_variant (GeminiQueryRow): The gemini variant
-                sample2idx (dict): A dictionary with sample_ids: index
+                individual_objs (list(dict)): A list of Individuals
             
             Returns:
                 individuals (list) A list of Genotypes
         """
         individuals = []
-        for sample_id in sample2idx:
-            index = sample2idx[sample_id]
+        for ind in individual_objs:
+            index = ind['index']
             individuals.append(Genotype(
-                sample_id=sample_id, 
+                sample_id=ind['ind_id'], 
                 genotype=gemini_variant['gts'][index], 
                 ref_depth=gemini_variant['gt_ref_depths'][index], 
                 alt_depth=gemini_variant['gt_alt_depths'][index],
@@ -99,7 +128,7 @@ class GeminiPlugin(Plugin):
             )
         return genes
     
-    def _get_transcripts(self, gemini_variant, gemini_db):
+    def _get_transcripts(self, gemini_variant):
         """Return a Transcript object
             
             Gemini stores the information for the most severe transcript
@@ -107,7 +136,6 @@ class GeminiPlugin(Plugin):
             
             Args:
                 gemini_variant (GeminiQueryRow): The gemini variant
-                gemini_db (str): Path to gemini db
             
             Returns:
                 transcripts list: List of affected transcripts
@@ -116,7 +144,7 @@ class GeminiPlugin(Plugin):
         query = "SELECT * from variant_impacts WHERE variant_id = {0}".format(
             gemini_variant['variant_id']
         )
-        gq = GeminiQuery(gemini_db)
+        gq = GeminiQuery(self.db)
         gq.run(query)
         
         transcripts = []
@@ -134,104 +162,155 @@ class GeminiPlugin(Plugin):
                 )
             )
         return transcripts
+    
+    def _is_variant(self, gemini_variant, indexes):
+        """Check if the variants is a variation in any of the individuals
+        
+            Args:
+                gemini_variant (GeminiQueryRow): The gemini variant
+                indexes (list(int)): A list of indexes for the individuals
+            
+            Returns:
+                bool : If any of the individuals has the variant
+        """
+        
+        for index in indexes:
+            if gemini_variant['gts'][index] != 0:
+                return True
+        
+        return False
 
-    def _variants(self, gemini_db, gemini_query=None):
-        """Return variants found in the gemini database"""
-        try:
-            gq = GeminiQuery(gemini_db)
-        except OperationalError as e:
-            message = "{0} is not a valid gemini db".format(gemini_db)
-            logger.error(message)
-            raise DatabaseError(message)
+    def _format_variant(self, gemini_variant, individual_objs, index=0):
+        """Make a puzzle variant from a gemini variant
+
+            Args:
+                gemini_variant (GeminiQueryRow): The gemini variant
+                individual_objs (list(dict)): A list of Individuals
+                index(int): The index of the variant
+            
+            Returns:
+                variant (dict): A Variant object
+        """
+        variant_dict = {
+            'CHROM':gemini_variant['chrom'].lstrip('chr'),
+            'POS':str(gemini_variant['start']),
+            'ID':gemini_variant['rs_ids'],
+            'REF':gemini_variant['ref'],
+            'ALT':gemini_variant['alt'],
+            'QUAL':gemini_variant['qual'],
+            'FILTER':gemini_variant['filter']
+        }
+    
+        variant = Variant(**variant_dict)
+        variant['index'] = index
         
-        sample2idx = gq.sample_to_idx
-        logger.info("Found samples: {0}".format(', '.join(sample2idx.keys())))
+        # Use the gemini id for fast search
+        variant.update_variant_id(gemini_variant['variant_id'])
+        # Update the individuals
+        individual_genotypes = self._get_individuals(
+            gemini_variant=gemini_variant, 
+            individual_objs=individual_objs
+            )
         
-        if not gemini_query:
-            gemini_query = "select * from variants"
+        for individual in individual_genotypes:
+            # Add the genotype calls to the variant
+            variant.add_individual(individual)
+    
+        for transcript in self._get_transcripts(gemini_variant):
+            variant.add_transcript(transcript)
+    
+        variant['most_severe_consequence'] = get_most_severe_consequence(
+            variant['transcripts']
+        )
+    
+        for gene in self._get_genes(variant):
+            variant.add_gene(gene)
+    
+        #### Check the impact annotations ####
+        if gemini_variant['cadd_scaled']:
+            variant['cadd_score'] = gemini_variant['cadd_scaled']
+    
+        # We use the prediction in text
+        polyphen = gemini_variant['polyphen_pred']
+        if polyphen:
+            variant.add_severity('Polyphen', polyphen)
+
+        # We use the prediction in text
+        sift = gemini_variant['sift_pred']
+        if sift:
+            variant.add_severity('SIFT', sift)
+    
+        #### Check the frequencies ####
+        thousand_g = gemini_variant['aaf_1kg_all']
+        if thousand_g:
+            variant['thousand_g'] = float(thousand_g)
+            variant.add_frequency(name='1000GAF', value=float(thousand_g))
+    
+        exac = gemini_variant['aaf_exac_all']
+        if exac:
+            variant.add_frequency(name='EXaC', value=float(exac))
+
+        esp = gemini_variant['aaf_esp_all']
+        if esp:
+            variant.add_frequency(name='ESP', value=float(esp))
+        
+        max_freq = gemini_variant['max_aaf_all']
+        if max_freq:
+            variant.set_max_freq(max_freq)
+        
+        return variant
+        
+    def _variants(self, case_id, gemini_query):
+        """Return variants found in the gemini database
+        
+            Args:
+                case_id (str): The case for which we want to see information
+                gemini_query (str): What variants should be chosen
+            
+            Yields:
+                variant_obj (dict): A Variant formatted doctionary
+        """
+        
+        gq = GeminiQuery(self.db)
         
         gq.run(gemini_query)
         
-        for index, gemini_variant in enumerate(gq):
-            index += 1
-            logger.debug("Updating index to: {0}".format(index))
-
-            # print("max_aaf_all {0}".format(gemini_variant['max_aaf_all']))
-
-            variant_dict = {
-                'CHROM':gemini_variant['chrom'].lstrip('chr'),
-                'POS':str(gemini_variant['start']),
-                'ID':gemini_variant['rs_ids'],
-                'REF':gemini_variant['ref'],
-                'ALT':gemini_variant['alt'],
-                'QUAL':gemini_variant['qual'],
-                'FILTER':gemini_variant['filter']
-            }
-            
-            variant = Variant(**variant_dict)
-            variant['index'] = index
-            # Use the gemini id for fast search
-            variant.update_variant_id(gemini_variant['variant_id'])
-            # Update the individuals
-            for individual in self._get_individuals(
-                gemini_variant=gemini_variant, sample2idx=sample2idx):
-                # Add the genotype calls to the variant
-                variant.add_individual(individual)
-            
-            for transcript in self._get_transcripts(gemini_variant, gemini_db):
-                variant.add_transcript(transcript)
-            
-            variant['most_severe_consequence'] = get_most_severe_consequence(
-                variant['transcripts']
-            )
-            
-            for gene in self._get_genes(variant):
-                variant.add_gene(gene)
-            
-            #### Check the impact annotations ####
-            if gemini_variant['cadd_scaled']:
-                variant['cadd_score'] = gemini_variant['cadd_scaled']
-            
-            # We use the prediction in text
-            polyphen = gemini_variant['polyphen_pred']
-            if polyphen:
-                variant.add_severity('Polyphen', polyphen)
-
-            # We use the prediction in text
-            sift = gemini_variant['sift_pred']
-            if sift:
-                variant.add_severity('SIFT', sift)
-            
-            #### Check the frequencies ####
-            thousand_g = gemini_variant['aaf_1kg_all']
-            if thousand_g:
-                variant['thousand_g'] = float(thousand_g)
-                variant.add_frequency(name='1000GAF', value=float(thousand_g))
-            
-            exac = gemini_variant['aaf_exac_all']
-            if exac:
-                variant.add_frequency(name='EXaC', value=float(exac))
-
-            esp = gemini_variant['aaf_esp_all']
-            if esp:
-                variant.add_frequency(name='ESP', value=float(esp))
-            
-            yield variant
+        individuals = []
+        # Get the individuals for the case
+        for case in self.cases():
+            if case['name'] == case_id:
+                for individual in case['individuals']:
+                    individuals.append(individual)
+        
+        indexes = [individual['index'] for individual in individuals]
+        
+        index = 0
+        for gemini_variant in gq:
+            # Check if variant is non ref in the individuals
+            if self._is_variant(gemini_variant, indexes):
+                index += 1
+                logger.debug("Updating index to: {0}".format(index))
+                
+                variant = self._format_variant(
+                    gemini_variant=gemini_variant, 
+                    individual_objs=individuals, 
+                    index=index
+                )
+                yield variant
             
         
     
     def variants(self, case_id, skip=0, count=30, gene_list=None, 
-                    thousand_g=None):
+                    frequency=None, cadd=None):
         """Return count variants for a case.
             
             case_id : A gemini db
             skip (int): Skip first variants
             count (int): The number of variants to return
             gene_list (list): A list of genes
-            thousand_g (float): filter variants based on frequency
+            frequency (float): filter variants based on frequency
             
         """
-        case_id = case_id.replace('|', '/')
         logger.debug("Looking for variants in {0}".format(case_id))
         
         limit = count + skip
@@ -240,26 +319,32 @@ class GeminiPlugin(Plugin):
         
         #This would be the fastest solution but it seems like we loose all variants
         #that are missing a frequency...
-        if thousand_g:
-            gemini_query += " WHERE (aaf_1kg_all < {0} or aaf_1kg_all is"\
-                            " Null)".format(thousand_g)
+        any_filter = None
+        if frequency:
+            gemini_query += " WHERE (max_aaf_all < {0} or max_aaf_all is"\
+                            " Null)".format(frequency)
+            any_filter = True
         
+        if cadd:
+            if any_filter:
+                gemini_query += " AND (cadd_scaled > {0})".format(cadd)
+            else:
+                gemini_query += " WHERE (cadd_scaled > {0})".format(cadd)
+            any_filter = True
+            
         filtered_variants = self._variants(
-            gemini_db=case_id, 
+            case_id=case_id,
             gemini_query=gemini_query)
         
         if gene_list:
             gene_list = set(gene_list)
             filtered_variants = (variant for variant in filtered_variants
-                                 if set(variant['hgnc_symbols'].intersection(gene_list)))
+                                 if (set(gene['symbol'] for gene in variant['genes'])
+                                     .intersection(gene_list)))
 
-        if thousand_g:
-            filtered_variants = (variant for variant in filtered_variants
-                                 if variant['thousand_g'] <= thousand_g)
-
-        for variant_obj in filtered_variants:
-            if variant_obj['index'] >= skip:
-                if variant_obj['index'] <= limit:
+        for index, variant_obj in enumerate(filtered_variants):
+            if index >= skip:
+                if index < limit:
                     yield variant_obj
                 else:
                     break
@@ -278,14 +363,28 @@ class GeminiPlugin(Plugin):
                 variant_obj (dict): A puzzle variant
                 
         """
-        case_id = case_id.replace('|', '/')
         variant_id = int(variant_id)
         gemini_query = "SELECT * from variants WHERE variant_id = {0}".format(
             variant_id
         )
-        for variant in self._variants(
-            gemini_db = case_id, 
-            gemini_query=gemini_query):
+        
+        individuals = []
+        # Get the individuals for the case
+        for case in self.cases():
+            if case['name'] == case_id:
+                for individual in case['individuals']:
+                    individuals.append(individual)
+        
+        gq = GeminiQuery(self.db)
+        
+        gq.run(gemini_query)
+        
+        for gemini_variant in gq:
+            variant = self._format_variant(
+                gemini_variant=gemini_variant, 
+                individual_objs=individuals, 
+                index=gemini_variant['variant_id']
+            )
             
             return variant
         
@@ -298,6 +397,7 @@ if __name__ == '__main__':
     configure_stream(level="DEBUG")
     gemini_db = sys.argv[1]
     plugin = GeminiPlugin()
+    
     for case in plugin.cases(gemini_db):
         print("Case found: {0}".format(case))
     # try:
